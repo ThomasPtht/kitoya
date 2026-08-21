9e8781c3b4caaa60c52a0636be0de51d8ae834aa
Thomas Potherat <t.potherat@laposte.net>
2026-08-21 08:48:15 +0200

    fix: remove stripe import in app module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -12,7 +12,6 @@ import { KotdModule } from './kotd/kotd.module';
 import { EmailService } from './email/email.service';
 import { PasswordResetService } from './password-reset/password-reset.service';
 import { PasswordResetModule } from './password-reset/password-reset.module';
-import { StripeModule } from './stripe/stripe.module';
 import { SubscriptionModule } from './subscription/subscription.module';
 import { GoogleStrategyService } from './google-strategy/google-strategy.service';
 import { GoogleStrategyController } from './google-strategy/google-strategy.controller';
@@ -34,7 +33,6 @@ import { FeedbackModule } from './feedback/feedback.module';
     ImageProcessingModule,
     KotdModule,
     PasswordResetModule,
-    StripeModule,
     SubscriptionModule,
     GoogleStrategyModule,
     LockerModule,
