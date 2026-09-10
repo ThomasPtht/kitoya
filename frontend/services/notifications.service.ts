9656b3fac2aaca46e528fa36246cd125bcbd16e0
Thomas Potherat <t.potherat@laposte.net>
2026-09-10 15:38:50 +0200

    remove deprecated notification prop

--- frontend/services/notifications.service.ts
+++ frontend/services/notifications.service.ts
@@ -7,7 +7,6 @@ import { router } from "expo-router";
 
 Notifications.setNotificationHandler({
   handleNotification: async () => ({
-    shouldShowAlert: true,
     shouldPlaySound: true,
     shouldSetBadge: true,
     shouldShowBanner: true,
